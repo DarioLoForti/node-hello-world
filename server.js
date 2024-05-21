bcfa81ac7c08d68a518be544ba2758ea51de6ffb
Dario Lo Forti <lofortidario@gmail.com>
2024-05-21 16:32:53 +0200

    add scafolding html

--- server.js
+++ server.js
@@ -30,7 +30,18 @@ const server = http.createServer((req, res) => {
     }
     res.writeHead(200, { "Content-Type": "text/html" });
     const randomQuote = getRandomQuote();
-    res.end(`<h1>${title}</h1><p>${randomQuote}</p>`);
+    res.end(`
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>${title}</h1><p>${randomQuote}</p>
+</body>
+</html>`);
 });
 
 server.listen(port, host, () => {
